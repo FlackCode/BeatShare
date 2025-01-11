b3d7a7a8aa0e63a1bcf685b19cba44770f4eef23
FlackCode <fl4ckc0de@gmail.com>
2025-01-11 20:39:18 +0200

    Init Commit

---
+++ app/routes/_index.tsx
@@ -0,0 +1,22 @@
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "BeatShare" },
+    { name: "description", content: "Welcome to BeatShare!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="flex flex-col items-center gap-16">
+        <header className="flex flex-col items-center gap-9">
+          <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
+            Welcome to <span>BeatShare</span>
+          </h1>
+        </header>
+      </div>
+    </div>
+  );
+}
